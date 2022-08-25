db6fee0d410b4a307d12caf90e97483d6c8bc4e6
Kássia <kassia_guterres@outlook.com>
2022-08-25 13:21:45 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+
+const server = express();
+
+const holidays = [
+  { date: "1/1/2022", name: "Confraternização mundial" },
+  { date: "1/3/2022", name: "Carnaval" },
+  //{ date: "8/25/2022", name: "kassia" },
+  { date: "4/17/2022", name: "Páscoa" },
+  { date: "4/21/2022", name: "Tiradentes" },
+  { date: "5/1/2022", name: "Dia do trabalho" },
+  { date: "6/16/2022", name: "Corpus Christi" },
+  { date: "9/7/2022", name: "Independência do Brasil" },
+  { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+  { date: "11/2/2022", name: "Finados" },
+  { date: "11/15/2022", name: "Proclamação da República" },
+  { date: "12/25/2022", name: "Natal" },
+];
+
+server.get("/holidays", (request, response) => {
+  response.send(holidays);
+});
+
+server.get("/is-today-holiday", (request, response) => {
+  const hoje = new Date();
+  let mensagem = `Não, hoje não é feriado`;
+  
+  let holiday = holidays.find(
+    (value) => value.date === hoje.toLocaleDateString("en-us")
+  );
+  if (holiday) {
+    mensagem = `Sim, hoje é nome-do-feriado`;
+  }
+  response.send(mensagem);
+});
+
+console.log(holidays);
+server.listen(4000, function () {
+  console.log(`Listen on 4000`);
+});
